5e89146dad86ecac02906b78f06b4d9a318ba2ef
jordyn <jordyn.kerrison@live.com.au>
2019-02-26 14:27:50 +1100

    create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<HTML>
+
+<header>
+    <title>Hello World</title>
+    
+</header>
+
+    <body>
+    
+            <?php echo "<h1>This is me</h1>" ?>
+    </body>
+
+    
+<footer>
+<?php echo "life could be fun for everyone"  ?>  
+</footer>
+
+
+
+
+
+
+</HTML>
